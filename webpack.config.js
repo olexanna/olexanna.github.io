dd2767a8767dc3fd773aae79051236af8710d0ee
Kengarut <kengarut@gmail.com>
2023-12-07 20:37:28 +0300

    new fix3

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
 	entry: "./source/main.js",
 	output: {
-		path: path.join(__dirname, "/release"),
+		path: path.join(__dirname, "/release/"),
 		filename: "release.js",
 		publicPath: "/release/"
 	},
